f8fd00c8070bd895914c8055a8cbec243e38c14e
Wade Forman <wforman099@gmail.com>
2017-01-25 00:20:38 -0500

    fixed untracked files

---
+++ app/the-fool/the_fool/urls.py
@@ -0,0 +1,24 @@
+"""
+Definition of urls for the_fool.
+"""
+
+from django.conf.urls import include, url
+from the_fool.app import views
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+#print("views", views.home)
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', views.current_datetime)
+    # url(r'^the_fool/', include('the_fool.the_fool.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
