953f0b7afec569742c9c7946f8d788dac410390b
Wade Forman <wforman099@gmail.com>
2017-01-26 20:00:46 -0500

    fixing gitignore

--- app/the-fool/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\wade.forman\Documents\bearly-software\the-fool\app\the-fool\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
