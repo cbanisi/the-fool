a282c75205acdec98461f0af5c25cd7e85b74e7c
Wade Forman <wforman099@gmail.com>
2017-01-26 20:46:12 -0500

    adds a /deck route

---
+++ app/the-fool/app/models.py
@@ -0,0 +1,10 @@
+#import collections
+
+class TarotCardDeck():
+
+    def __init__(self):
+        self.deck = [];
+
+    def shuffle(self):
+        self.deck.append("Card 1")
+        return self.deck
