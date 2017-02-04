4ec522bf408f4c996e126a71fafbecf879b9f390
Wade Forman <wforman099@gmail.com>
2017-02-04 18:56:07 -0500

    fix spacing

--- app/the-fool/the_fool/tests/TarotCardDeckTest.py
+++ app/the-fool/the_fool/tests/TarotCardDeckTest.py
@@ -9,22 +9,22 @@ class TarotCardDeckTestCase(TestCase):
     
     def test_you_can_create_TarotCardDeck(self):
         tarotCardDeck = TarotCardDeck()
-        self.assertEquals(22,len(tarotCardDeck.deck))
+        self.assertEqual(22,len(tarotCardDeck.deck))
 
     def test_shuffle_returns_deck(self):
         tarotCardDeck = TarotCardDeck()
         self.assertEqual(len(tarotCardDeck.shuffle()),22)
 
     def test_card_names_are_unique(self):
-	tarotCardDeck = TarotCardDeck()
-	name_set = set()
-	for card in tarotCardDeck.deck:
-	    name_set.add(card.name)
-	self.assertEquals(len(name_set),22)
+        tarotCardDeck = TarotCardDeck()
+        name_set = set()
+        for card in tarotCardDeck.deck:
+            name_set.add(card.name)
+        self.assertEqual(len(name_set),22)
 
     def test_card_definitions_are_unique(self):
-	tarotCardDeck = TarotCardDeck()
-	description_set = set()
-	for card in tarotCardDeck.deck:
-	    description_set.add(card.description)
-	self.assertEquals(len(description_set),22)
+        tarotCardDeck = TarotCardDeck()
+        description_set = set()
+        for card in tarotCardDeck.deck:
+            description_set.add(card.description)
+        self.assertEqual(len(description_set),22)
